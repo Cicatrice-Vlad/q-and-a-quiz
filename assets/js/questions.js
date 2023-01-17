dcfaf773ae8ed676142af1d85761c761ef9ee651
Cicatrice-Vlad <vladbargaoanu@yahoo.com>
2023-01-17 23:42:54 +0000

    adding some variables of the whole quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,10 @@
 console.log("QUESTIONS");
 
-var myQuestions = "audi";
+var myQuestionNumber3 = "Coding Quiz Challenge";
+var myQuestionNumber1 = "Arrays in JavaScript can be used to store_______";
+var myQuestionNumber2 =
+  "A very useful tool during development and debugging for printing code to the debugger is _______";
+var myFinishLine = "All Done!";
 
 if (myCar === "audi") {
   console.log("Whatever you want");
